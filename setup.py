8977e42450a3fb94d301f9bb8d93a75f0d058643
fitnr <fitnr@fakeisthenewreal>
2014-10-19 00:57:21 -0400

    fix package hyphens

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(
-    name="python_date_utils",
+    name="python-date-utils",
     version="2.0",
     description="Python Date Utils package",
     long_description="Converts between Gregorian dates and other calender systems. These calendars are included: Baha'i, French Republican, Hebrew, Indian Civil, Islamic, Julian, Mayana and Persian.",
