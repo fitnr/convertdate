4ce38fecc40f9f3d0e4879494ab62b578eacd945
fitnr <fitnr@fakeisthenewreal>
2014-11-15 19:37:07 -0500

    clean up tests

--- tests/babylon_test.py
+++ tests/babylon_test.py
@@ -3,9 +3,12 @@ from copy import copy
 import unittest
 from convertdate import dublin
 from convertdate import julian
+from convertdate import gregorian
 from convertdate import babylonian as bab
 from convertdate.data import babylonian_data as data
 import ephem
+from random import randint
+from math import trunc
 
 
 class test_babylon_cal(unittest.TestCase):
@@ -47,8 +50,8 @@ class test_babylon_cal(unittest.TestCase):
 
         assert bab.intercalation_pattern('U') == {
             1: u'Nisannu', 2: u'Aiaru', 3: u'Simanu', 4: u'Duzu', 5: u'Abu', 6: u'Ululu',
-            7: u'Ululu II', 8: u'Tashritu', 9: u'Araḥsamnu', 10: u'Kislimu', 11: u'Ṭebetu',
-            12: u'Shabaṭu', 13: u'Addaru'}
+            7: u'Ululu II', 8: u'Tashritu', 9: u'Araá¸¥samnu', 10: u'Kislimu', 11: u'á¹¬ebetu',
+            12: u'Shabaá¹­u', 13: u'Addaru'}
 
         assert len(leapyear_U) == 13
         assert leapyear_U[7] == u"Ululu II"
@@ -103,15 +106,15 @@ class test_babylon_cal(unittest.TestCase):
 
     def test_prev_visible_nm(self):
         dc = dublin.from_gregorian(2014, 11, 25)
-        self.assertEqual(bab.previous_visible_nm(dc).tuple(), (2014, 11, 23, 0, 0, 0))
+        self.assertEqual(bab.previous_visible_nm(dc).tuple(), (2014, 11, 22, 0, 0, 0))
 
     def test_babylon_from_jd_analeptic(self):
-        assert bab.from_julian(46, 3, 27, 'seleucid') == (356, u'Addaru', 30)
+        self.assertEqual(bab.from_julian(46, 3, 27, 'seleucid'), (357, u'Nisannu', 1))
 
-        assert bab.from_julian(100, 3, 2) == (410, u'Addaru', 2)
-        assert bab.from_julian(100, 4, 2) == (411, u'Nisannu', 3)
-        assert bab.from_julian(100, 5, 2) == (411, u'Aiaru', 3)
-        assert bab.from_julian(100, 6, 2) == (411, u'Simanu', 5)
+        self.assertEqual(bab.from_julian(100, 3, 2), (410, u'Addaru', 3))
+        self.assertEqual(bab.from_julian(100, 4, 2), (411, u'Nisannu', 4))
+        self.assertEqual(bab.from_julian(100, 5, 1), (411, u'Aiaru', 4))
+        self.assertEqual(bab.from_julian(100, 6, 1), (411, u'Simanu', 5))
 
         cjs = [
             1738298,
@@ -138,7 +141,7 @@ class test_babylon_cal(unittest.TestCase):
             if one[2] not in [28, 29, 30]:
                 assert one[2] + 1 == two[2]
 
-        assert bab.from_gregorian(2014, 11, 7, plain=1) == (2325, 'Arahsamnu', 14)
+        self.assertEqual(bab.from_gregorian(2014, 11, 7, plain=1), (2325, 'Arahsamnu', 15))
 
     def test_load_parker_dubberstein(self):
         bab.load_parker_dubberstein()
@@ -147,16 +150,37 @@ class test_babylon_cal(unittest.TestCase):
         assert parkerdub[-604].get('months')
         assert parkerdub[-581].get('months').get(6) == julian.to_jd(-581, 9, 12)
 
-        # print '\nyear\tmonth\tday\tM\tpve\tnm\n'
-        # for x in range(-626, -311):
-    def test_year_lengths(self):
+    def test_year_lengths_parker_dubberstein(self):
         parkerdub = bab.PARKER_DUBBERSTEIN
 
-        #     try:
-        #         start_o_year = parkerdub[x]['months'][1]
-        #         thing(start_o_year)
-        #     except KeyError:
-        #         pass
+        for year in list(range(-311, -1)) + list(range(1, 41)):
+            diy = days_in_year(parkerdub, year)
+
+            monlen = len(bab.intercalate(year))
+
+            self.assertGreater(diy, monlen * 29)
+            self.assertLess(diy, monlen * 30)
+
+    def test_year_starts(self):
+        for x in range(1700, 2000):
+            pev = bab._nvnm_after_pve(ephem.Date(str(x) + '/6/1'))
+            nex = bab._nvnm_after_pve(ephem.Date(str(x + 1) + '/6/1'))
+
+            diy = round(nex - pev, 1)
+            monlen = len(bab.intercalate(x))
+
+            self.assertGreater(diy, monlen * 29)
+            self.assertLess(diy, monlen * 30)
+
+
+    def test_year_lengths_analeptic(self):
+        for year in range(2000, 2200):
+            start = bab.to_jd(year, 'Nisannu', 1)
+            monlen = len(bab.intercalate(gregorian.from_jd(start)[0]))
+            end = bab.to_jd(year, monlen, 30)
+
+            self.assertLess(end - start, monlen * 30)
+            self.assertGreater(end - start, monlen * 29)
 
     def test_metonic_cycle(self):
         dc = dublin.from_gregorian(1900, 3, 19)
@@ -210,61 +234,153 @@ class test_babylon_cal(unittest.TestCase):
         z = gregorian.to_jd(1900, 3, 19)
 
         for x in range(0, 1000, 20):
-            try:
-                w = bab.to_jd(*bab.from_jd(z + x))
-                print(u"{}\t{}\t{}\t{}\t{}\t{}".format((z + x), w, w - z - x, *bab.from_jd(z + x, plain=1)))
+            w = bab.to_jd(*bab.from_jd(z + x))
+                # print((u"{}\t{}\t{}\t{}\t{}\t{}".format((z + x), w, w - z - x, *bab.from_jd(z + x, plain=1))))
 
-                assert (z + x) == bab.to_jd(*bab.from_jd(z + x))
+            self.assertEqual((z + x), bab.to_jd(*bab.from_jd(z + x)))
 
-            except AssertionError:
-                pass
         assert bab.to_jd(*bab.from_jd(z)) == z
-    def test_len_years(self):
-        for x in range(1900, 2000):
-            pev = bab._nvnm_after_pve(ephem.Date(str(x) + '/6/1'))
-            nex = bab._nvnm_after_pve(ephem.Date(str(x + 1) + '/6/1'))
 
-            yearlen = round(nex - pev, 1)
-            assert yearlen >= 353.0
+    # def test_limits_of_metonic(self):
+    #     print('test_limits_of_metonic')
+    #     for y in list(range(-595, 0, 38)) + list(range(14, 4000, 38)):
+    #         vnm = bab._nvnm_after_pve(ephem.Date(str(y) + '/6/1'))
+    #         print('*', y, round(vnm - ephem.previous_vernal_equinox(str(y) + '/6/1')), bab.from_gregorian(y, vnm.datetime().month, vnm.datetime().day))
 
-            if len(bab.intercalate(x)) == 12:
-                assert yearlen < 355.5
+    #     moon = ephem.next_new_moon('128/4/15')
+    #     for x in range(1, 237):
+    #         pve = ephem.previous_vernal_equinox(moon)
+    #         if moon - pve < 29.5:
+    #             v = pve
+    #         else:
+    #             v = ''
 
-            if len(bab.intercalate(x)) == 13:
-                assert yearlen < 385.5
-                try:
-                    assert yearlen > 382.5
-                except:
-                    print('xxx', x, yearlen)
+    #         m = bab.metonic_number(moon.datetime().year)
+
+    #         if moon.datetime().year > pve.datetime().year:
+    #             m -= 1
+
+    #         # print("{}\t{}\t{}\t{}".format(x, m, moon, v))
+    #         moon = ephem.next_new_moon(moon)
+
+    # def test_na1(self):
+    #     for u, v in d:
+    #         print u, v
+
+    #     print('nvnm', bab.next_visible_nm(ephem.Date('2014/3/19')))
+
+    #     babylon = bab.observer()
+    #     sun = ephem.Sun()
+    #     moon = ephem.Moon()
+    #     print('moons NA1')
+    #     nnm = ephem.next_new_moon(1)
+
+    #     print('x, moon.alt, sun.alt')
+    #     for x in range(1, 100):
+    #         babylon.date = nnm = ephem.next_new_moon(nnm)
+    #         babylon.date = babylon.next_setting(sun)
+    #         moon.compute(babylon)
+
+    #         if moon.alt < 0:
+    #             babylon.date = babylon.next_setting(sun)
+    #             print('moon alt %s' % round(moon.alt * 57.2957795))
+
+    #     print("{}\t{}\t{}".format(x, round(moon.alt * 57.2957795), round(sun.az * 57.2957795)))
 
     def test_overall_days_of_month(self):
-        r = randint(1757584, 2450544)
 
+        # nnm = ephem.next_new_moon(10)
+
+        # for x in range(1, 100):
+        #     pnm = nnm
+        #     nnm = ephem.next_new_moon(pnm)
+        #     pvnm = bab.previous_visible_nm(nnm)
+        #     nvnm = bab.next_visible_nm(pnm)
+        #     print(round(nnm - pnm, 2), round(nvnm - pvnm, 2))
+
+        r = randint(1757584, 2450544)
         for x in range(r, r + 10000, 30):
             dc = dublin.from_jd(x)
             nvnm = bab.next_visible_nm(dc)
             pvnm = bab.previous_visible_nm(dc)
-            try:
-                assert round(nvnm - pvnm, 0) in [29.0, 30.0]
-            except AssertionError:
-                print('AssertionError', nvnm, pvnm, round(nvnm - pvnm, 0))
-                print('previous NM', ephem.previous_new_moon(dc))
-                print('next NM', ephem.next_new_moon(dc))
+            assert round(nvnm - pvnm, 0) in [29.0, 30.0, 29, 30]
 
 
 def thing(jdc):
-    dc = dublin.from_jd(jdc)
-    pve = ephem.previous_vernal_equinox(dc)
-    days_since_pve = dc - pve
-    days_since_nm = dc - ephem.previous_new_moon(dc)
+    '''jdc should be the first day of a bab year'''
     jy, jm, jd = julian.from_jd(jdc)
+    dc = dublin.from_jd(jdc)
+    midyear = dublin.from_julian(jy, 5, 1)
 
-    print("{year}\t{month}\t{day}\t{M}\t{pve}\t{nm}".format(
-        year=jy, month=jm, day=jd,
-        pve=int(days_since_pve),
-        nm=int(days_since_nm),
-        M=bab.metonic_number(jy)
-    ))
+    days_since_ve = dc - ephem.previous_vernal_equinox(midyear)
+    days_since_nm = dc - ephem.previous_new_moon(dc)
+
+    return {
+        'jyear': jy,
+        'jdate': '{}-{}'.format(jm, jd),
+        've': round(days_since_ve, 2),
+        'nm': round(days_since_nm, 2),
+        'm': bab.metonic_number(jy)
+    }
+
+
+def analeptic_start_of_year(gyear):
+    ddate = ephem.Date(str(gyear) + '/5/1')
+    nnm = bab._nvnm_after_pve(ddate + 3)
+    babdate = bab.from_gregorian(*dublin.to_gregorian(nnm))
+
+    if babdate[1] != 'Nisannu':
+        nnm = nnm + 30
+        babdate = bab.from_gregorian(*dublin.to_gregorian(nnm), plain=1)
+
+    while babdate[2] != 1:
+        if babdate[2] > 1:
+            nnm = nnm - 1
+            babdate = bab.from_gregorian(*dublin.to_gregorian(nnm), plain=1)
+
+    return dublin.to_jd(nnm)
+
+
+def analeptic_thing(gyear):
+    return thing(analeptic_start_of_year(gyear))
+
+
+def days_in_year(parkerdub, jyear):
+    '''Return the number of days in the bab. year that overlaps this JY'''
+    return parkerdub[jyear + 1]['months'][1] - parkerdub[jyear]['months'][1]
+
+
+def _analeptic_days_in_year(gyear):
+    month = len(bab.intercalate(gyear))
+    firstmoon = moon = bab._nvnm_after_pve(ephem.Date(str(gyear) + '/5/1'))
+
+    for x in range(1, 1 + month):
+        # print x, moon
+        moon = ephem.next_new_moon(moon)
+
+    return round(moon - firstmoon)
+
+
+def days_btw_ve(jyear):
+    yearve = ephem.previous_vernal_equinox(dublin.from_julian(jyear, 6, 6))
+    nextve = ephem.previous_vernal_equinox(dublin.from_julian(jyear + 1, 6, 6))
+    return nextve - yearve
+
+
+def count_moons_between_ve(startyear):
+    '''Starting at a year, count the number of moons between vernal equinoxes for next 19 years'''
+    startdate = ephem.Date(str(startyear) + '/1/1')
+    enddate = ephem.Date(str(startyear + 19) + '/1/1')
+
+    ve = ephem.next_vernal_equinox(startdate)
+    years = {}
+
+    while ve < enddate:
+        nve = ephem.next_vernal_equinox(ve + 1)
+        years[ve.datetime().year] = bab.moons_between_dates(ve, nve)
+        ve = nve
+
+    return years
 
 
 def count_pattern(startingve):
