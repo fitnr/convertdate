cb030e6a226f5fb71f81448265738d1c73264039
Berkeley Churchill (on remembrance) <berkeley@berkeleychurchill.com>
2019-05-02 13:46:20 -0700

    adding new file for testing bahai dates

---
+++ tests/test_bahai.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import unittest
+import time
+from convertdate import gregorian
+from convertdate import bahai
+
+
+class TestBahai(unittest.TestCase):
+
+    def setUp(self):
+        self.tm = time.localtime()
+        self.gregoriandate = (self.tm[0], self.tm[1], self.tm[2])
+
+    def trivial(self):
+        assert 1 == 1
+
+
+if __name__ == '__main__':
+    unittest.main()
