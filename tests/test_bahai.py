316839d96e7031049f5e662c9ab78d807876e14c
Berkeley Churchill (on remembrance) <berkeley@berkeleychurchill.com>
2019-05-02 14:02:04 -0700

    fixing pymeeus version, adding nawruz tests

--- tests/test_bahai.py
+++ tests/test_bahai.py
@@ -15,6 +15,24 @@ class TestBahai(unittest.TestCase):
     def test_trivial(self):
         assert 1 == 1
 
+    def test_nawruz(self):
+        nawruz_official = {
+            2015: 21,
+            2016: 20,
+            2017: 20,
+            2018: 21,
+            2019: 21,
+            2020: 20,
+            2021: 20
+        }
+
+        for year in nawruz_official:
+            bahaiyear = year - 1844 + 1
+            (gregorian_year, gregorian_month, gregorian_day) = bahai.to_gregorian(bahaiyear, 1, 1)
+            assert gregorian_year == year
+            assert gregorian_month == 3
+            assert gregorian_day == nawruz_official[year]
+
 
 if __name__ == '__main__':
     unittest.main()
