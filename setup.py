c8ef653a39d4f13472439df43656b2606368c07e
fitnr <fitnr@fakeisthenewreal>
2019-10-26 12:31:25 -0400

    add version 3.8 support, remove 3.3

--- setup.py
+++ setup.py
@@ -54,6 +54,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Religion',
         'Topic :: Scientific/Engineering :: Astronomy',
         'Operating System :: OS Independent',
