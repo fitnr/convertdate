7d7ae2143dca5bee71188df282b9c4e16455f7ef
fitnr <fitnr@fakeisthenewreal>
2020-09-27 16:33:38 -0400

    remove python2, 3.4 support
    adjust pythons version in Travis
    
    allow python3.5

--- setup.py
+++ setup.py
@@ -41,10 +41,7 @@ setup(
     zip_safe=True,
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -54,6 +51,7 @@ setup(
         'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License'
     ],
+    python_requires=">=3.5",
     install_requires=[
         'pytz>=2014.10',
         'pymeeus>=0.3.6, <=1'
