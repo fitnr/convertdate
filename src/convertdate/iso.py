3cec65b48cbc45b8f3142e49b86a8a84dd428b36
fitnr <fitnr@fakeisthenewreal>
2020-11-01 15:05:21 -0500

    use builtins where available

--- src/convertdate/iso.py
+++ src/convertdate/iso.py
@@ -5,6 +5,7 @@
 # http://opensource.org/licenses/MIT
 # Copyright (c) 2016, fitnr <fitnr@fakeisthenewreal>
 from calendar import isleap
+from datetime import date
 from math import trunc
 
 from . import gregorian, ordinal
@@ -26,23 +27,7 @@ def to_jd(year, week, day):
 
 def from_jd(jd):
     '''Return tuple of ISO (year, week, day) for Julian day'''
-    year = gregorian.from_jd(jd)[0]
-    day = jwday(jd) + 1
-
-    dayofyear = ordinal.from_jd(jd)[1]
-    week = trunc((dayofyear - day + 10) / 7)
-
-    # Reset year
-    if week < 1:
-        week = weeks_per_year(year - 1)
-        year = year - 1
-
-    # Check that year actually has 53 weeks
-    elif week == 53 and weeks_per_year(year) != 53:
-        week = 1
-        year = year + 1
-
-    return year, week, day
+    return from_gregorian(*gregorian.from_jd(jd))
 
 
 def weeks_per_year(year):
@@ -57,7 +42,7 @@ def weeks_per_year(year):
 
 
 def from_gregorian(year, month, day):
-    return from_jd(gregorian.to_jd(year, month, day))
+    return date(year, month, day).isocalendar()
 
 
 def to_gregorian(year, week, day):
