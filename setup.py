0863a582dd01d6dc47acf94a676d27caa8e75964
Alex DelVecchio <31746850+javulticat@users.noreply.github.com>
2021-02-16 16:37:31 -0500

    Avoid broken pymeeus release 0.3.8

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     python_requires=">=3.5",
     install_requires=[
         'pytz>=2014.10',
-        'pymeeus>=0.3.6, <=1'
+        'pymeeus>=0.3.6, <=1, !=0.3.8'
     ],
     tests_require=tests_require,
     extras_require={
